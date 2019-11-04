8d45165d9bf2100b147ae841eb13bacdd261c284
Niklas Kühtmann <nk@ized.io>
2019-11-05 00:19:28 +0100

    Improve restypings

--- packages/backend/src/typed-router.d.ts
+++ packages/backend/src/typed-router.d.ts
@@ -10,12 +10,14 @@ type GetRoute<R extends Record<string, any>> = {
     params: R["params"];
 }
 
+type Handler<S = any, C = {}, R = {}> = (context: Omit<Koa.ParameterizedContext<S, C>, keyof R>&R, next: () => Promise<any>) => any;
+
 declare type TypedRouter<Schema extends RestypedBase = any, S = any, C = {}> = Omit<Router<S, C>, HTTPMethods> & {
-    get<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, GetRoute<Schema[Path]["GET"]>>): TypedRouter<Schema, S, C>;
-    post<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["POST"]>): TypedRouter<Schema, S, C>;
-    put<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["PUT"]>): TypedRouter<Schema, S, C>;
-    patch<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["PATCH"]>): TypedRouter<Schema, S, C>;
-    head<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["HEAD"]>): TypedRouter<Schema, S, C>;
-    delete<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["DELETE"]>): TypedRouter<Schema, S, C>;
-    options<Path extends keyof Schema>(path: Path, handler: Koa.Middleware<S, C & Schema[Path]["OPTIONS"]>): TypedRouter<Schema, S, C>;
+    get<Path extends keyof Schema>(path: Path, handler: Handler<S, C, GetRoute<Schema[Path]["GET"]>>): TypedRouter<Schema, S, C>;
+    post<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["POST"]>): TypedRouter<Schema, S, C>;
+    put<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["PUT"]>): TypedRouter<Schema, S, C>;
+    patch<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["PATCH"]>): TypedRouter<Schema, S, C>;
+    head<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["HEAD"]>): TypedRouter<Schema, S, C>;
+    delete<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["DELETE"]>): TypedRouter<Schema, S, C>;
+    options<Path extends keyof Schema>(path: Path, handler: Handler<S, C, Schema[Path]["OPTIONS"]>): TypedRouter<Schema, S, C>;
 }

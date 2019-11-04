2da354389f1fe29214761dd15163d8c8c854e7f6
Niklas Kühtmann <nk@ized.io>
2019-11-04 22:38:14 +0100

    Add koa server

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,37 @@
+import Koa from "koa";
+import Router from "koa-router";
+import logger from "koa-logger";
+import json from "koa-json";
 
 import {helloWorld} from "@kas/shared";
 
+
 export async function main()
 {
+    const app = new Koa();
+    const router = await createRouter();
+    const port = parseInt(process.env.PORT) || 3000;
+
+    app.use(json());
+    app.use(logger());
+
+    app.use(router.routes()).use(router.allowedMethods());
+
+    app.listen(port, () => {
+        console.log(`Server started on port ${port}`);
+    })
+
     console.log(helloWorld);
+}
+
+async function createRouter()
+{
+    const router = new Router();
+
+    router.get("/", async (ctx, next) => {
+        ctx.body = { msgFromShared: helloWorld }
+        await next();
+    });
+
+    return router;
 }

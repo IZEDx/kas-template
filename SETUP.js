e3b3402c6bda528fa48ae505d41cc97d3c8d0435
Niklas Kühtmann <nk@ized.io>
2019-11-09 04:24:21 +0100

    Add setup logic

--- SETUP.js
+++ SETUP.js
@@ -1,5 +1,7 @@
 
 var readline = require('readline');
+var fs = require("fs");
+var path = require("path");
 
 process.stdout.write("Name: ");
 var rl = readline.createInterface({
@@ -8,7 +10,55 @@ var rl = readline.createInterface({
     terminal: false
 });
 
-rl.on('line', function(line){
-    console.log(line);
+let step = 0;
+rl.on('line', line => {
+    switch (step++) {
+        case 0: return setup(line);
+        case 1: return cleanup(line);
+    }
+});
+
+const filesToRename = {
+    "kas-template.code-workspace": (name) => name+".code-workspace"
+}
+
+const replaceKasTemplate = {
+    "kas-template": (name) => name
+};
+
+const filesToModify = {
+    "README.md": replaceKasTemplate
+}
+
+function setup(name)
+{
+    for (const file of Object.entries(filesToRename)) {
+        const from = file[0];
+        const to = file[1](name);
+
+        console.log("Renaming", from, "to", to);
+        fs.renameSync(path.join(__dirname, from), path.join(__dirname, to));
+    }
+
+    for (const file of Object.entries(filesToModify)) {
+        const filePath = path.join(__dirname, file[0]);
+        let content = fs.readFileSync(filePath).toString();
+
+        for (const rule of Object.entries(file[1])) {
+            content = content.replace(new RegExp(rule[0], "g"), rule[1](name));
+        }
+
+        console.log("Updating", file[0]);
+        fs.writeFileSync(filePath, content);
+    }
+}
+
+process.stdout.write("Delete SETUP file? (y/n): ");
+function cleanup(delSetup = "n")
+{
+    if (delSetup.toLowerCase().startsWith("y")) {
+        fs.unlinkSync(path.join(__dirname, "SETUP.js"));
+    }
+
     process.exit();
-})
+}
